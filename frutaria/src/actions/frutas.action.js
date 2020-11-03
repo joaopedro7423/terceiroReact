404bd4471b93d4cc47a940212ec3a3960092baf2
Joao Pedro <802.018@alunos.unigran.br>
2020-11-03 08:12:18 -0300

    Actions

---
+++ frutaria/src/actions/frutas.action.js
@@ -0,0 +1,14 @@
+
+
+const actions = {
+    adicionar: fruta =>({
+        type:'ADICIONAR_FRUTA',
+        payload: fruta,
+    }),
+    remover: fruta =>({
+        type:'REMOVER_FRUTA',
+        payload:fruta,
+    }),
+};
+
+export {actions};
